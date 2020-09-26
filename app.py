129b22c4ac661a6e2c9aeb731726794b1ce9a67e
Winn <winnieee.lam00@gmail.com>
2020-09-25 20:59:32 -0400

    Added CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/get_img_urls/', methods=['GET'])
